25bada96b6ab553675a5e463f903b64b2254ae6e
Closertech001 <danielajayi842@gmail.com>
2025-07-19 12:06:34 -0500

    Update memory.py

--- utils/memory.py
+++ utils/memory.py
@@ -1,9 +1,10 @@
 import streamlit as st
+import uuid
 
-def init_memory():
-    if "chat_history" not in st.session_state:
+def init_memory(reset=False):
+    if reset or "chat_history" not in st.session_state:
         st.session_state.chat_history = []
-    if "related_questions" not in st.session_state:
+    if reset or "related_questions" not in st.session_state:
         st.session_state.related_questions = []
-    if "last_department" not in st.session_state:
+    if reset or "last_department" not in st.session_state:
         st.session_state.last_department = None

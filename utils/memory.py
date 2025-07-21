f76b240b2263da1356171a7166962c0910156900
Closertech001 <danielajayi842@gmail.com>
2025-07-21 15:21:31 -0500

    Delete utils/memory.py

--- utils/memory.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def init_memory():
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = []
-    if "related_questions" not in st.session_state:
-        st.session_state.related_questions = []
-    if "last_department" not in st.session_state:
-        st.session_state.last_department = None

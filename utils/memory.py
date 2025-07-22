077b00240b084819ee7f629a4cc6f6cc9433e753
Closertech001 <danielajayi842@gmail.com>
2025-07-22 17:35:08 -0500

    Update memory.py

--- utils/memory.py
+++ utils/memory.py
@@ -1,36 +1,19 @@
 # utils/memory.py
 
-class Memory:
-    """
-    Tracks conversational memory like department and level.
-    """
+class MemoryHandler:
     def __init__(self):
-        self.context = {
-            "department": None,
-            "level": None,
-            "semester": None
-        }
+        self.last_department = None
+        self.last_level = None
+        self.last_topic = None
 
-    def update(self, **kwargs):
-        """
-        Update memory context. Accepts any of: department, level, semester.
-        """
-        for key in kwargs:
-            if key in self.context:
-                self.context[key] = kwargs[key]
+    def update(self, department=None, level=None):
+        if department:
+            self.last_department = department
+        if level:
+            self.last_level = level
+
+    def update_last_topic(self, topic):
+        self.last_topic = topic
 
     def get(self):
-        """
-        Returns current memory values as a dict.
-        """
-        return self.context.copy()
-
-    def reset(self):
-        """
-        Resets the memory to default state.
-        """
-        for key in self.context:
-            self.context[key] = None
-
-    def __str__(self):
-        return f"Memory({self.context})"
+        return self.last_department, self.last_level, self.last_topic

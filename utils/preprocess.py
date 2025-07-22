1ccdc9c31473e976f568a970d8b774250dd28483
Closertech001 <danielajayi842@gmail.com>
2025-07-22 15:42:59 -0500

    Update preprocess.py

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,50 +1,34 @@
 import re
+import streamlit as st
+from symspellpy import SymSpell, Verbosity
+import pkg_resources
 
-# --- Phrase replacements ---
-PHRASE_REPLACEMENTS = {
-    "biz admin": "business administration",
-    "mass comm": "mass communication",
-    "cresnt uni": "crescent university",
-    "cresnt": "crescent",
-    "final year": "final level",
-    "last year": "final level",
-    "yr 1": "100 level",
-    "yr 2": "200 level",
-    "yr 3": "300 level",
-    "yr 4": "400 level",
-    "yr 5": "500 level",
-    "year 1": "100 level",
-    "year 2": "200 level",
-    "year 3": "300 level",
-    "year 4": "400 level",
-    "year 5": "500 level",
-    "1st year": "100 level",
-    "2nd year": "200 level",
-    "3rd year": "300 level",
-    "4th year": "400 level",
-    "5th year": "500 level"
-}
-
-# --- Abbreviations ---
+# Abbreviations for shorthand normalization
 ABBREVIATIONS = {
     "u": "you", "r": "are", "ur": "your", "cn": "can", "cud": "could",
     "shud": "should", "wud": "would", "abt": "about", "bcz": "because",
     "plz": "please", "pls": "please", "tmrw": "tomorrow", "wat": "what",
     "wats": "what is", "info": "information", "yr": "year", "sem": "semester",
     "admsn": "admission", "clg": "college", "sch": "school", "uni": "university",
-    "l": "level", "d": "the", "msg": "message", "dept": "department",
-    "reg": "registration", "fee": "fees", "pg": "postgraduate", "app": "application",
-    "req": "requirement", "nd": "national diploma", "2nd": "second", "1st": "first",
-    "nxt": "next", "prev": "previous", "exp": "experience"
+    "cresnt": "crescent", "l": "level", "d": "the", "msg": "message",
+    "idk": "i don't know", "imo": "in my opinion", "asap": "as soon as possible",
+    "dept": "department", "reg": "registration", "fee": "fees", "pg": "postgraduate",
+    "app": "application", "req": "requirement", "nd": "national diploma",
+    "a-level": "advanced level", "alevel": "advanced level", "2nd": "second",
+    "1st": "first", "nxt": "next", "prev": "previous", "exp": "experience",
+    "CSC": "department of Computer Science", "Mass comm": "department of Mass Communication",
+    "law": "department of law", "Acc": "department of Accounting"
 }
 
-# --- Synonyms ---
+# Synonyms to help semantic matching
 SYNONYMS = {
-    "lecturers": "academic staff", "professors": "academic staff", "teachers": "academic staff",
-    "instructors": "academic staff", "tutors": "academic staff", "staff members": "staff",
-    "head": "dean", "hod": "head of department", "dept": "department", "school": "university",
-    "college": "faculty", "course": "subject", "class": "course", "subject": "course",
-    "unit": "credit", "credit unit": "unit", "course load": "unit", "non teaching": "non-academic",
+    "lecturers": "academic staff", "professors": "academic staff",
+    "teachers": "academic staff", "instructors": "academic staff",
+    "tutors": "academic staff", "staff members": "staff",
+    "head": "dean", "hod": "head of department", "dept": "department",
+    "school": "university", "college": "faculty", "course": "subject",
+    "class": "course", "subject": "course", "unit": "credit",
+    "credit unit": "unit", "course load": "unit", "non teaching": "non-academic",
     "admin worker": "non-academic staff", "support staff": "non-academic staff",
     "clerk": "non-academic staff", "receptionist": "non-academic staff",
     "secretary": "non-academic staff", "tech staff": "technical staff",
@@ -55,39 +39,31 @@ SYNONYMS = {
     "needed for": "required for", "who handles": "who manages"
 }
 
-# --- Pidgin normalization ---
-PIDGIN_MAP = {
-    "wetin": "what", "dey": "is", "una": "you all", "na": "is",
-    "abi": "right", "wey": "that", "fit": "can", "go do": "will do",
-    "wan": "want", "no get": "don't have", "dey go": "are going",
-    "comot": "leave", "carry go": "take away", "waka": "walk"
-}
+def get_sym_spell():
+    sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
+    dictionary_path = pkg_resources.resource_filename("symspellpy", "frequency_dictionary_en_82_765.txt")
+    sym_spell.load_dictionary(dictionary_path, term_index=0, count_index=1)
+    return sym_spell
 
-# --- Repeated character reducer (e.g., "heyyyyy" -> "hey") ---
-def reduce_repeated_chars(text: str) -> str:
-    return re.sub(r'(.)\1{2,}', r'\1', text)
+def apply_abbreviations(words):
+    return [ABBREVIATIONS.get(word, word) for word in words]
 
-# --- Main normalization function ---
-def normalize_input(text: str) -> str:
-    # Step 1: Basic cleanup
-    text = reduce_repeated_chars(text.lower().strip())
+def apply_synonyms(words):
+    return [SYNONYMS.get(word, word) for word in words]
 
-    # Step 2: Phrase-level replacements
-    for phrase, replacement in PHRASE_REPLACEMENTS.items():
-        text = re.sub(rf"\b{re.escape(phrase)}\b", replacement, text)
+def normalize_text(text):
+    text = text.lower()
+    text = re.sub(r"[^\w\s]", "", text)
+    return text
 
-    # Step 3: Tokenize and replace
-    tokens = re.findall(r"\b\w+\b", text)
-    normalized = []
-
-    for token in tokens:
-        if token in ABBREVIATIONS:
-            normalized.append(ABBREVIATIONS[token])
-        elif token in SYNONYMS:
-            normalized.append(SYNONYMS[token])
-        elif token in PIDGIN_MAP:
-            normalized.append(PIDGIN_MAP[token])
-        else:
-            normalized.append(token)
-
-    return " ".join(normalized)
+def normalize_input(text):
+    text = normalize_text(text)
+    words = text.split()
+    words = apply_abbreviations(words)
+    sym_spell = get_sym_spell()
+    corrected = [
+        sym_spell.lookup(w, Verbosity.CLOSEST, 2)[0].term if sym_spell.lookup(w, Verbosity.CLOSEST, 2) else w
+        for w in words
+    ]
+    final = apply_synonyms(corrected)
+    return " ".join(final)

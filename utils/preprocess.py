6064eced127a952df9bc1204f1b7e16edf123ff7
Closertech001 <danielajayi842@gmail.com>
2025-07-22 08:52:07 -0500

    Update preprocess.py

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,41 +1,64 @@
 import re
-from symspellpy.symspellpy import SymSpell, Verbosity
+from symspellpy import SymSpell, Verbosity
 import pkg_resources
 
-# Load SymSpell
-sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
-dictionary_path = pkg_resources.resource_filename("symspellpy", "frequency_dictionary_en_82_765.txt")
-sym_spell.load_dictionary(dictionary_path, term_index=0, count_index=1)
-
-# Abbreviation and slang map
-NORMALIZATION_MAP = {
-    "uni": "university",
-    "cuab": "crescent university",
-    "dept": "department",
-    "course code": "course",
-    "abt": "about",
-    "info": "information",
-    "u": "you",
-    "ur": "your",
-    "wat": "what",
-    "wats": "what is",
-    "pls": "please",
-    "plz": "please",
-    "cos": "because",
-    "bcos": "because",
-    "4": "for",
-    "2": "to",
-    "1st": "first",
-    "2nd": "second",
-    "3rd": "third",
-    "hw": "how"
+ABBREVIATIONS = {
+    "comp sci": "computer science",
+    "com sci": "computer science",
+    "compsci": "computer science",
+    "mass comm": "mass communication",
+    "masscom": "mass communication",
+    "med lab": "medical laboratory science",
+    "medlab": "medical laboratory science",
+    "nurs": "nursing",
+    "nursin": "nursing",
+    "biz admin": "business administration",
+    "int'l rel": "international relations",
+    "int rel": "international relations",
+    "irs": "international relations",
+    "econs": "economics with operations research",
+    "pol sci": "political science",
+    "pis": "political science",
+    "mls": "medical laboratory science",
+    "cs": "computer science",
+    "ir": "international relations",
 }
 
-def normalize_input(text: str) -> str:
+SYNONYMS = {
+    "dept": "department",
+    "uni": "university",
+    "school": "university",
+    "courses": "course",
+    "requirement": "requirements",
+    "admission": "admission",
+    "fees": "fee",
+}
+
+def get_sym_spell():
+    sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
+    dictionary_path = pkg_resources.resource_filename("symspellpy", "frequency_dictionary_en_82_765.txt")
+    sym_spell.load_dictionary(dictionary_path, term_index=0, count_index=1)
+    return sym_spell
+
+def apply_abbreviations(words):
+    return [ABBREVIATIONS.get(word, word) for word in words]
+
+def apply_synonyms(words):
+    return [SYNONYMS.get(word, word) for word in words]
+
+def normalize_text(text):
     text = text.lower()
-    for short, full in NORMALIZATION_MAP.items():
-        text = re.sub(rf"\b{short}\b", full, text)
-    suggestions = sym_spell.lookup_compound(text, max_edit_distance=2)
-    if suggestions:
-        return suggestions[0].term
+    text = re.sub(r"[^\w\s]", "", text)
     return text
+
+def normalize_input(text):
+    text = normalize_text(text)
+    words = text.split()
+    words = apply_abbreviations(words)
+    sym_spell = get_sym_spell()
+    corrected = [
+        sym_spell.lookup(w, Verbosity.CLOSEST, 2)[0].term if sym_spell.lookup(w, Verbosity.CLOSEST, 2) else w
+        for w in words
+    ]
+    final = apply_synonyms(corrected)
+    return " ".join(final)

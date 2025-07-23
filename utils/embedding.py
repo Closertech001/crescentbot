89266dd45d6622aad4009b0687888768f577a1e3
Closertech001 <danielajayi842@gmail.com>
2025-07-23 04:46:10 -0500

    Update embedding.py

--- utils/embedding.py
+++ utils/embedding.py
@@ -4,16 +4,15 @@ from sentence_transformers import SentenceTransformer
 import torch
 
 def load_model(model_name="all-MiniLM-L6-v2"):
+    """Load SentenceTransformer model"""
     return SentenceTransformer(model_name)
 
 def load_dataset(path="data/crescent_qa.json"):
+    """Load Q&A dataset from JSON into pandas DataFrame"""
     with open(path, "r", encoding="utf-8") as f:
         data = json.load(f)
     return pd.DataFrame(data)
 
-# This alias is added in case some other file tries to import `load_data`
-load_data = load_dataset
-
 def compute_question_embeddings(questions, model):
-    embeddings = model.encode(questions, convert_to_tensor=True, show_progress_bar=True)
-    return embeddings
+    """Compute sentence embeddings for a list of questions"""
+    return model.encode(questions, convert_to_tensor=True, show_progress_bar=True)

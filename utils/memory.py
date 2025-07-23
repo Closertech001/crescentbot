0b4b5821732427487c290a51f07058c8a071385b
Closertech001 <danielajayi842@gmail.com>
2025-07-23 04:10:16 -0500

    Update memory.py

--- utils/memory.py
+++ utils/memory.py
@@ -1,25 +1,8 @@
-# utils/memory.py
+# Memory handler using Streamlit session_state
+import streamlit as st
 
-def update_last_query_context(session_state, query_info):
-    """
-    Save the latest query context (department, level, semester) into Streamlit session state
-    """
-    if "last_query_info" not in session_state:
-        session_state["last_query_info"] = {}
+def get_last_query_info():
+    return st.session_state.get("last_query_info", {})
 
-    if query_info.get("department"):
-        session_state["last_query_info"]["department"] = query_info["department"]
-        session_state["last_query_info"]["faculty"] = query_info.get("faculty")
-
-    if query_info.get("level"):
-        session_state["last_query_info"]["level"] = query_info["level"]
-
-    if query_info.get("semester"):
-        session_state["last_query_info"]["semester"] = query_info["semester"]
-
-
-def get_last_query_context(session_state):
-    """
-    Retrieve the last query context (department, level, semester) from memory
-    """
-    return session_state.get("last_query_info", {})
+def set_last_query_info(info):
+    st.session_state["last_query_info"] = info

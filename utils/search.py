615f231436b9ea459f430428b9554b692b8eb924
Closertech001 <danielajayi842@gmail.com>
2025-07-23 04:32:58 -0500

    Update search.py

--- utils/search.py
+++ utils/search.py
@@ -1,8 +1,8 @@
-from sentence_transformers.util import cos_sim
 import torch
+from sentence_transformers.util import cos_sim
 
-def semantic_search(query, model, embeddings, df, threshold=0.6):
-    user_embedding = model.encode(query.strip().lower(), convert_to_tensor=True)
+def find_response(user_question, model, embeddings, df, threshold=0.6):
+    user_embedding = model.encode(user_question.strip().lower(), convert_to_tensor=True)
     cosine_scores = cos_sim(user_embedding, embeddings)[0]
     best_score = torch.max(cosine_scores).item()
     best_idx = torch.argmax(cosine_scores).item()
